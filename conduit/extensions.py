1b6c992fc65b62fa3d1ba0f515ab665c771ce16e
Alberto Vara <alberto.vara@datadoghq.com>
2023-10-10 15:03:13 +0200

    feat(appsec): security endpoints (#2)
    * feat(appsec): add appsec views
    
    * chore: update requirements

--- conduit/extensions.py
+++ conduit/extensions.py
@@ -6,8 +6,8 @@ from flask_caching import Cache
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy, Model
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy.model import Model
 
 class CRUDMixin(Model):
     """Mixin that adds convenience methods for CRUD (create, read, update, delete) operations."""
---
+++ conduit/security/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""Datadog Security Views module."""
+from . import views  # noqa
