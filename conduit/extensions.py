bc6d719339de3bceacd1cc4f198fba933770fa45
ZStriker19 <zach.groves@datadoghq.com>
2023-10-20 09:14:02 -0400

    fix requirements for 3.12

--- conduit/extensions.py
+++ conduit/extensions.py
@@ -6,7 +6,8 @@ from flask_caching import Cache
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy, Model
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy.model import Model
 
 
 class CRUDMixin(Model):
